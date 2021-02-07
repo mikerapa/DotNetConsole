6cd8aea139f16b36e3db6eb99985b5a0ea27aa04
Mike Rapa <mike@mikerapa.com>
2021-02-06 21:25:43 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Con3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
